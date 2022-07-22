5989c28f4fd0c792a8e1d8e43859297aef151586
LucasF <lucasfreitas286@outlook.com>
2022-07-22 12:36:16 -0300

    inclusao api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mensagemErro = document.querySelector("#mensagemErro");
 const cepbusca = document.querySelector("#cep");
+const numero = document.querySelector("#numero");
 const logradouro = document.querySelector("#logradouro");
 const bairro = document.querySelector("#bairro");
 const localidade = document.querySelector("#localidade");
@@ -12,9 +13,9 @@ let endereco;
 btnBuscar.addEventListener('click', (e) => {
     e.preventDefault(); 
     try{
-        //validaCEP();
+        validaCEP();
     }catch(erro){
-        //mensagemErro.innerHTML = erro.message;
+        mensagemErro.innerHTML = erro.message;
     }
 })
 
@@ -22,18 +23,63 @@ btnLimpar.addEventListener('click', () => {
     limparCampos();
 })
 
-btnSalvar.addEventListener('click', () => {
-    alert("Dados salvos com sucesso!!!");
-    limparCampos();
+btnSalvar.addEventListener('click', (e) => {
+    e.preventDefault();
+    try{
+        salvaDados();
+    }catch(erro){
+        mensagemErro.innerHTML = erro.message;
+    }
 })
 
-function limpaCampos(){
+function buscaEndereco(){
+    fetch(`http://viacep.com.br/ws/${cepbusca.value}/json/`)
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((endereco) => {
+            preencheEndereço(endereco);
+    })
+    .catch((erro) => {
+        console.log(erro);
+    })
+}
+
+function preencheEndereço(end){
+    for(const campo in end){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = end[campo];
+        }
+    }
+}
+
+function limparCampos(){
     cepbusca.value = "";
     logradouro.value = "";
+    numero.value = "";
     bairro.value = "";
     localidade.value = "";
     uf.value = "";
     mensagemErro.innerHTML = "";
+}
+
+function validaCEP(){
+    const regex = /^\d{5}[-]\d{3}$/;
+    if(regex.test(cepbusca.value)){
+        mensagemErro.innerHTML = '';
+        buscaEndereco();
+    }  else{
+        throw new Error("CEP inválido");
+    }
+}
+
+function salvaDados(){
+    if(numero.value === ''){
+        throw new Error("Campo número não preenchido!");
+    }else{
+        alert("Dados salvos com sucesso!");
+        limparCampos();
+    }
 }
 
 
